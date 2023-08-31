3d18eba4c9ea6a2e02ea97f2ecfbbf92891af27c
a-ramji <anna@ramji.org>
2023-08-31 18:12:14 +0000

    added reading_data.R, testing_graph1.qmd

---
+++ R/reading_data.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(janitor)
+
+PRM_data <- read_csv("/courses/EDS214/group_project/A1/LEF_data/RioMameyesPuenteRoto.csv") 
+BQ1_data <- read_csv("/courses/EDS214/group_project/A1/LEF_data/QuebradaCuenca1-Bisley.csv") 
+BQ2_data <- read_csv("/courses/EDS214/group_project/A1/LEF_data/QuebradaCuenca2-Bisley.csv") 
+BQ3_data <- read_csv("/courses/EDS214/group_project/A1/LEF_data/QuebradaCuenca3-Bisley.csv") 
